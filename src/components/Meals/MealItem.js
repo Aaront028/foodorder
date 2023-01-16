7fd46da7ea3bfa7241a1655823b501b3e13750ea
Aaron Tan <info@aarontan.co.nz>
2023-01-16 17:35:03 +1300

    MealItem css

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -2,13 +2,17 @@ import React from 'react'
 import classes from './MealItem.module.css'
 
 function MealItem(props) {
+  const price = `$${props.price.toFixed(2)}`
+
   return (
-    <>
-      <div className={classes.meal}>{props.name}</div>
-      <h3 className={classes.meal}>{props.name}</h3>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{props.price}</div>
-    </>
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
   )
 }
 
