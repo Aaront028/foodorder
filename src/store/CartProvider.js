b1f48326e21eaa7eda908934580c22c9605155e9
Aaron Tan <info@aarontan.co.nz>
2023-01-17 15:08:17 +1300

    add useReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,41 @@
+import { useReducer } from 'react'
+
 import CartContext from './cart-context'
 
-function CartProvider(props) {
-  const addItemToCartHandler = (item) => {}
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+}
 
-  const removeItemFromCartHandler = (id) => {}
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
+  return defaultCartState
+}
+
+function CartProvider(props) {
+  const [cartState, dispatchCartAtion] = useReducer(
+    cartReducer,
+    defaultCartState
+  )
+  const addItemToCartHandler = (item) => {
+    dispatchCartAtion({ type: 'ADD', item: item })
+  }
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAtion({ type: 'REMOVE', id: id })
+  }
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   }
